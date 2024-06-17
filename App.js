7ced0dcd6df99c5b8d271d9d8c519b28c68f8634
adhu23520 <adhuvaa5@gmail.com>
2024-06-18 02:53:09 +0300

    actual backend push

--- App.js
+++ App.js
@@ -5,29 +5,54 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5/Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
-//import modules from "./Kanbas/Database/modules.js";
+import modules from "./Kanbas/Database/modules.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import UserRoutes from "./User/routes.js";
-
-
-
+import session from "express-session";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+
 mongoose.connect(CONNECTION_STRING);
 
-
-
 const app = express();
-app.use(cors());
+
+// var cors = require('cors');    
+// app.use(cors({credentials: true, origin: process.env.NETLIFY_URL || 'http://localhost:3000'}));
+
+app.use(cors({
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    credentials: true  
+})
+);
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+
+
 app.use(express.json());
+const port = process.env.PORT || 4000;
 
 Hello(app);
 Lab5(app);
+app.listen(process.env.PORT || 4000);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 UserRoutes(app);
 
 
-app.listen(process.env.PORT || 4000)
+
--- User/routes.js
+++ User/routes.js
@@ -1,17 +1,23 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+
 export default function UserRoutes(app) {
+
+    const signout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+
 
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
         res.json(user);
-      };
-    
+    };
 
-    const deleteUser = async (req, res) => { 
+
+    const deleteUser = async (req, res) => {
         const status = await dao.deleteUser(req.params.userId);
-      res.json(status);
-     };
+        res.json(status);
+    };
 
     const findAllUsers = async (req, res) => {
         const { role, name } = req.query;
@@ -44,12 +50,46 @@ export default function UserRoutes(app) {
         const { userId } = req.params;
         const status = await dao.updateUser(userId, req.body);
         res.json(status);
-      };
-    
-    const signup = async (req, res) => { };
-    const signin = async (req, res) => { };
-    const signout = (req, res) => { };
-    const profile = async (req, res) => { };
+    };
+
+    const signup = async (req, res) => {
+        const user = await dao.findUserByUsername(req.body.username);
+        if (user) {
+            res.status(400).json({ message: "Username already taken" });
+            return;
+        }
+        const currentUser = await dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
+        res.json(currentUser);
+    };
+
+
+
+    const signin = async (req, res) => {
+        const { username, password } = req.body;
+        const currentUser = await dao.findUserByCredentials(username, password);
+        if (currentUser) {
+            req.session["currentUser"] = currentUser;
+            res.json(currentUser);
+        } else {
+            res.status(401).json({ message: "Unable to login. Try again later." });
+        }
+    };
+
+
+
+
+    const profile = (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+            res.sendStatus(401);
+            return;
+        }
+        res.json(currentUser);
+    };
+
+
+
 
 
     app.post("/api/users", createUser);
